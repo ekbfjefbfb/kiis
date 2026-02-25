d4d010db5479bc668a77eabe4ffaf800355afaef
ekbfjefbfb <orbelinanavarro2@gmail.com>
2026-02-25 15:54:07 -0600

    fix: fatal crash on startup removing notesService.init() and add netlify _redirects

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-
-  import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-import { notesService } from "./services/notes.service";
-
-// Initialize services
-notesService.init().catch(console.error);
 
 createRoot(document.getElementById("root")!).render(<App />);
   
